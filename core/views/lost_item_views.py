50a8783201381455b3610c0aae6668d86519f577
Hans Queja <hansqueja8@gmail.com>
2025-04-08 15:55:11 +0800

    Feat: Add delete method for lost items.

--- core/views/lost_item_views.py
+++ core/views/lost_item_views.py
@@ -1,10 +1,21 @@
-from django.core.serializers import serialize
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from core.models import LostItem
 
 # Lost Items GET
 def get_lost_items(request):
-    items = LostItem.objects.all()
-    data = serialize("json", items)
-    return HttpResponse(data, content_type="application/json")
+    items = LostItem.objects.all().values()
+    return JsonResponse(list(items), status=200, safe=False)
+
+# Lost Items DELETE
+@csrf_exempt
+def delete_lost_items(request, item_id):
+    try:
+        item = LostItem.objects.get(id=item_id)
+        if item.delete():
+            return JsonResponse({"message": "Item deleted."}, status=200)
+        else:
+            return JsonResponse({"message": "Error deleting item."}, status=405)
+    except LostItem.DoesNotExist:
+         return JsonResponse({"message": "Item does not exists."}, status=404)
