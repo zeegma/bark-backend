b3d4e87037384e990decb51f04d950c06e550b54
Hans Queja <hansqueja8@gmail.com>
2025-04-08 12:53:19 +0800

    Feat: Add script to migrate and seed the database

---
+++ scripts/migrate_and_seed.py
@@ -0,0 +1,10 @@
+import os
+
+def run(*args):
+    # Perform migrations
+    os.system("python manage.py migrate")
+    
+    # Perform data seeding
+    os.system("python manage.py loaddata fixtures/admin_data.json")
+    os.system("python manage.py loaddata fixtures/item_data.json")
+    os.system("python manage.py loaddata fixtures/claim_data.json")
