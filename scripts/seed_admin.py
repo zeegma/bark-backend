bac7e810828bf51ec6f51197a68b6d5db6207162
Hans Queja <hansqueja8@gmail.com>
2025-04-08 12:53:37 +0800

    Feat: Add script to migrate and seed admin data.

---
+++ scripts/seed_admin.py
@@ -0,0 +1,8 @@
+import os
+
+def run(*args):
+    # Perform migrations
+    os.system("python manage.py migrate")
+    
+    # Perform data seeding
+    os.system("python manage.py loaddata fixtures/admin_data.json")
