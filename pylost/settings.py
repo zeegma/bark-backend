edc8c0f103d6e03977140b09b463f042c848d783
Hans Queja <hansqueja8@gmail.com>
2025-04-08 15:12:16 +0800

    Chore: Add backend endpoint to CORS_ALLOWED_ORIGINS.

--- pylost/settings.py
+++ pylost/settings.py
@@ -50,7 +50,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000"
 ]
 
 ROOT_URLCONF = 'pylost.urls'
