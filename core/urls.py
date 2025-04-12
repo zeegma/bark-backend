4372126c8c569d6434f896e03de2e1866631befd
Hans Queja <hansqueja8@gmail.com>
2025-04-12 09:27:16 +0800

    Refactor: Add edit lost item API endpoint.

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views.lost_item_views import get_lost_items, create_lost_items, delete_lost_items
+from .views.lost_item_views import get_lost_items, create_lost_items, edit_lost_items, delete_lost_items
 
 from . import views
 
 urlpatterns = [
     path("", get_lost_items, name="getLostItems"),
-    path("lost-items", get_lost_items, name="getLostItems"),
-    path("lost-items/create", create_lost_items, name="createLostItems"),
-    path("lost-items/<int:item_id>", delete_lost_items, name="deleteLostItems"),
+    path("lost-items/", get_lost_items, name="getLostItems"),
+    path("lost-items/create/", create_lost_items, name="createLostItems"),
+    path("lost-items/<int:item_id>/", edit_lost_items, name="editLostItems"),
+    path("lost-items/<int:item_id>/delete/", delete_lost_items, name="deleteLostItems"),
 ]
