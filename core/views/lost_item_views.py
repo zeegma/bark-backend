13e8878dc6202704aab488f79166fcee917b3a27
Hans Queja <hansqueja8@gmail.com>
2025-04-12 22:45:28 +0800

    Refactor: Move the helper functions to designated file.

--- core/views/lost_item_views.py
+++ core/views/lost_item_views.py
@@ -1,12 +1,9 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-import os
-import re
 import json
-from supabase import create_client, Client
-from datetime import datetime
 
+from .helpers.item_helpers import upload_photo_supabase
 from core.models import LostItem
 
 # Lost Items GET
@@ -67,60 +64,3 @@ def delete_lost_items(request, item_id):
     except LostItem.DoesNotExist:
          return JsonResponse({"message": "Item does not exists."}, status=404)
     
-
-# Creates a supabase connection
-def create_supabase_instance():
-    url: str = os.environ.get('SUPABASE_URL')
-    key: str = os.environ.get('SUPABASE_KEY')
-
-    supabase: Client = create_client(url, key)
-
-    return supabase
-
-
-# Uploads image from request to supabase
-def upload_photo_supabase(image):
-    supabase = create_supabase_instance()
-    image_data = image.read()
-
-    # Get last uploaded image name
-    response = (
-        supabase.storage
-        .from_("lost-item-images")
-        .list(
-            "",
-            {
-                "limit": 1,
-                "offset": 0,
-                "sortBy": {"column": "name", "order": "desc"},
-            }
-        )
-    )
-
-    # Get the name of the last item, and find what numbers it has
-    item_count = re.findall(r'\d+', response[0].get('name'))
-
-    # Convert said count to integer
-    item_count = int(item_count[0])
-
-    # Create string name with prefix, item count + 1, and content_type
-    image_name = "image_" + str(item_count + 1) + "." + image.content_type.split('/')[1]
-
-    # Upload image to supabase
-    try:
-        supabase.storage.from_("lost-item-images").upload(
-                file=image_data,
-                path=image_name,
-                file_options={"content-type": image.content_type}
-            )
-    except:
-        return -1
-    
-    # Takes the working URL of the recently uploaded image for storing
-    url_response = (
-        supabase.storage
-        .from_("lost-tiem-images")
-        .get_public_url(image.name)
-    )
-
-    return url_response
