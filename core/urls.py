9c84aeeab7c0f865c73211126c2618bd2de4e3f5
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-04-13 18:36:00 +0800

    Added GET endpoint for claim forms and successfully test retrieval

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views.lost_item_views import get_lost_items, create_lost_items, delete_lost_items
-from .views.claim_form_views import create_claim_form
+from .views.claim_form_views import create_claim_form, get_claim_forms
 
 urlpatterns = [
     path("", get_lost_items, name="getLostItems"),
     path("lost-items", get_lost_items, name="getLostItems"),
     path("lost-items/create", create_lost_items, name="createLostItems"),
     path("lost-items/<int:item_id>", delete_lost_items, name="deleteLostItems"),
-    path("claim-form/create", create_claim_form, name="create_claim_form"),
+    # Claim Form
+    path("claim-forms", get_claim_forms, name="getClaimForms"),
+    path("claim-form/create", create_claim_form, name="createClaimForm"),
 ]
--- core/views/claim_form_views.py
+++ core/views/claim_form_views.py
@@ -9,6 +9,13 @@ from datetime import datetime
 
 from core.models import LostItem, ClaimForm
 
+# Claim Form GET
+def get_claim_forms(request):
+
+    items = ClaimForm.objects.all().values()
+    return JsonResponse(list(items), status=200, safe=False)
+
+# Claim Form POST
 @csrf_exempt
 def create_claim_form(request):
     if request.method != 'POST':
@@ -39,3 +46,62 @@ def create_claim_form(request):
 
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
+    
+
+
+
+# # Create a supabase connection
+# def create_supabase_instance():
+#     url: str = os.environ.get('SUPABASE_URL')
+#     key: str = os.environ.get('SUPABASE_KEY')
+
+#     supabase: Client = create_client(url, key)
+
+#     return supabase
+
+# # Upload photo to Supabase and return URL
+# def upload_photo_supabase(image):
+#     supabase = create_supabase_instance()
+#     image_data = image.read()
+
+#     # Get last uploaded image name
+#     response = (
+#         supabase.storage
+#         .from_("claim-form-photos")
+#         .list(
+#             "",
+#             {
+#                 "limit": 1,
+#                 "offset": 0,
+#                 "sortBy": {"column": "name", "order": "desc"},
+#             }
+#         )
+#     )
+
+#     # Get the name of the last item and find what numbers it has
+#     item_count = re.findall(r'\d+', response[0].get('name')) if response else [0]
+
+#     # Convert said count to integer and increment for new file
+#     item_count = int(item_count[0])
+
+#     # Create a new image name using item count + 1
+#     image_name = "ownership_photo_" + str(item_count + 1) + "." + image.content_type.split('/')[1]
+
+#     # Upload the image to Supabase
+#     try:
+#         supabase.storage.from_("claim-form-photos").upload(
+#                 file=image_data,
+#                 path=image_name,
+#                 file_options={"content-type": image.content_type}
+#             )
+#     except:
+#         return -1
+
+#     # Get the public URL of the uploaded image
+#     url_response = (
+#         supabase.storage
+#         .from_("claim-form-photos")
+#         .get_public_url(image_name)
+#     )
+
+#     return url_response
