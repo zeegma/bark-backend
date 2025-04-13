73a7feb67bb9fc3b71ff13db9cd5ceafd671d501
sykeruzn <syruzkendomingo.c@gmail.com>
2025-04-13 16:33:13 +0800

    feat: Add POST endpoint for admin account creation and implement create_admin function

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.lost_item_views import get_lost_items, create_lost_items, delete_lost_items
-from .views.admin_views import get_admins
+from .views.admin_views import get_admins, create_admin
 
 from . import views
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path("lost-items/create", create_lost_items, name="createLostItems"),
     path("lost-items/<int:item_id>", delete_lost_items, name="deleteLostItems"),
 
-    path("admins", get_admins, name="getAdmins")
+    path("admins", get_admins, name="getAdmins"),
+    path("admins/create", create_admin, name="createAdmin")
 ]
--- core/views/admin_views.py
+++ core/views/admin_views.py
@@ -1,9 +1,38 @@
 from django.core.serializers import serialize
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from core.models import Admin
+
+import json
 
 # Admins GET
 def get_admins(request):
     items = Admin.objects.all().values()
     return JsonResponse(list(items), status=200, safe=False)
+
+# Admin POST: Create new admin account
+@csrf_exempt
+def create_admin(request):
+    if request.method != 'POST':
+        return JsonResponse({"message": "Only POST allowed."}, status=405)
+    
+    try:
+        data = json.loads(request.body)
+
+        # Checks if email already exists
+        if Admin.objects.filter(email = data.get('email')).exists():
+            return JsonResponse({"message": "Account with this email already exists."})
+        
+        # Create new admin
+        admin = Admin(
+            name = data.get('name'),
+            email = data.get('email'),
+            number = data.get('number'),
+            password = data.get('password')
+        )
+        admin.save()
+        return JsonResponse({"message": "Account successfully created."}, status=201)
+
+    except Exception as e:
+        return JsonResponse({"message": f"Error creating admin: {str(e)}"}, status=400)
