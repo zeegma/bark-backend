564351a860953d3e4d12b64c65aeb01c3ff476d2
sykeruzn <syruzkendomingo.c@gmail.com>
2025-04-16 12:20:55 +0800

    refactor: Add trailing slashes to admin endpoints and change delete_admin to admin_detail

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.lost_item_views import get_lost_items, create_lost_items, delete_lost_items
-from .views.admin_views import get_admins, register_admin, delete_admin, login_admin
+from .views.admin_views import get_admins, register_admin, admin_detail, login_admin
 
 from . import views
 
@@ -10,8 +10,8 @@ urlpatterns = [
     path("lost-items/create", create_lost_items, name="createLostItems"),
     path("lost-items/<int:item_id>", delete_lost_items, name="deleteLostItems"),
 
-    path("admins", get_admins, name="getAdmins"),
-    path("admins/register", register_admin, name="registerAdmin"),
-    path("admins/<int:admin_id>", delete_admin, name="deleteAdmin"),
-    path("admins/login", login_admin, name="loginAdmin")
+    path("admins/", get_admins, name="getAdmins"),
+    path("admins/register/", register_admin, name="registerAdmin"),
+    path("admins/<int:admin_id>", admin_detail, name="deleteAdmin"),
+    path("admins/login/", login_admin, name="loginAdmin")
 ]
