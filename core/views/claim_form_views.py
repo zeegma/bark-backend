5f21d794f231dd2ea4f7eb2512caf6ad2e20e70a
Hans Queja <hansqueja8@gmail.com>
2025-04-16 22:57:57 +0800

    Refactor: Adjust create method for claim forms.

--- core/views/claim_form_views.py
+++ core/views/claim_form_views.py
@@ -1,11 +1,8 @@
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
-import os
-import re
 import json
-from supabase import create_client, Client
-from datetime import datetime
+
+from .helpers.item_helpers import upload_photo_supabase, delete_photo_supabase
 
 from core.models import LostItem, ClaimForm
 
@@ -22,86 +19,23 @@ def create_claim_form(request):
         return JsonResponse({"message": "Only POST allowed."}, status=405)
 
     try:
-        # Parse JSON from the request body
-        data = json.loads(request.body)
+        # Processes the image, uploads to Supabase
+        # Also returns the url of the image from Supabase
+        image_url = upload_photo_supabase(request.FILES['image'], bucket="ownership-photo")
 
-        # Get item instance
-        try:
-            item = LostItem.objects.get(id=data.get('item_id'))
-        except LostItem.DoesNotExist:
-            return JsonResponse({"message": "Lost item not found."}, status=404)
+        # If same item or same name exists in database
+        if image_url == -1:
+            return JsonResponse({"message": "Item name already exists in database."}, status=405)
 
-        # Create a new ClaimForm
-        claim = ClaimForm.objects.create(
-            request_date=data.get('request_date'),
-            name=data.get('name'),
-            ownership_photo=data.get('ownership_photo'),
-            detailed_description=data.get('detailed_description'),
-            number=data.get('number'),
-            media=data.get('media'),
-            item_id=item
-        )
+        # Convert JSON string to dictionary
+        data = json.loads(request.POST['data'])
+        data['ownership_photo'] = image_url
+        data['item_id'] = LostItem.objects.get(id=data['item_id'])
+
+        claim = ClaimForm(**data)
+        claim.save()
 
         return JsonResponse({"message": "Claim form submitted successfully."}, status=201)
 
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
-    
-
-
-
-# # Create a supabase connection
-# def create_supabase_instance():
-#     url: str = os.environ.get('SUPABASE_URL')
-#     key: str = os.environ.get('SUPABASE_KEY')
-
-#     supabase: Client = create_client(url, key)
-
-#     return supabase
-
-# # Upload photo to Supabase and return URL
-# def upload_photo_supabase(image):
-#     supabase = create_supabase_instance()
-#     image_data = image.read()
-
-#     # Get last uploaded image name
-#     response = (
-#         supabase.storage
-#         .from_("claim-form-photos")
-#         .list(
-#             "",
-#             {
-#                 "limit": 1,
-#                 "offset": 0,
-#                 "sortBy": {"column": "name", "order": "desc"},
-#             }
-#         )
-#     )
-
-#     # Get the name of the last item and find what numbers it has
-#     item_count = re.findall(r'\d+', response[0].get('name')) if response else [0]
-
-#     # Convert said count to integer and increment for new file
-#     item_count = int(item_count[0])
-
-#     # Create a new image name using item count + 1
-#     image_name = "ownership_photo_" + str(item_count + 1) + "." + image.content_type.split('/')[1]
-
-#     # Upload the image to Supabase
-#     try:
-#         supabase.storage.from_("claim-form-photos").upload(
-#                 file=image_data,
-#                 path=image_name,
-#                 file_options={"content-type": image.content_type}
-#             )
-#     except:
-#         return -1
-
-#     # Get the public URL of the uploaded image
-#     url_response = (
-#         supabase.storage
-#         .from_("claim-form-photos")
-#         .get_public_url(image_name)
-#     )
-
-#     return url_response
