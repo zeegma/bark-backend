9a7099b117d3d633d8b95c4e2aa84edd80abde12
sykeruzn <syruzkendomingo.c@gmail.com>
2025-04-30 21:46:19 +0800

    Add JWT token endpoints to URL configuration

--- pylost/urls.py
+++ pylost/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("", include("core.urls"))
+    path("", include("core.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
